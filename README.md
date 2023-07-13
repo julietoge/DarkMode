d972b0793aac11633a5eacb7268e7ed88d7fcd68
julietoge <julietogechi27@gmail.com>
2023-07-13 16:16:54 +0100

    updated README.md

---
+++ README.md
@@ -0,0 +1,15 @@
+# MY PORTFOLIO
+## APP
+### Header
+#### Logo
+#### HeadersectionsLink
+
+### MainSection
+#### Introduction
+#### About
+#### Skills & Tools
+#### My projects
+#### Contact me
+### Footer
+#### FootersectionsLink
+#### copyWrite
--- src/pages/mainsection/contact/socialMediaHandles.jsx
+++ src/pages/mainsection/contact/socialMediaHandles.jsx
@@ -5,23 +5,23 @@ const SocialMediaHandles = () => {
   return (
     <div className="socialMediaHandles-Wrapper">
         <div className="linkedIn-Wrapper">
-        <a href="http://" target="_blank" rel="noopener noreferrer">
+        <a href="https://www.linkedin.com/in/ogechi-juliet-uhegbu" target="_blank" rel="noopener noreferrer">
         <img src="linkedIn" alt="LinkedIn" />
       </a>
         </div>
       
         <div className="linkedIn-Wrapper">
-        <a href="http://" target="_blank" rel="noopener noreferrer">
-        <img src="Email" alt="Email" />
-      </a>
+          <a href="mailto:julietogech127@gmail.com" target="_blank" rel="noopener noreferrer">
+          <img src="Email" alt="Email" />
+          </a>
         </div>
         <div className="linkedIn-Wrapper">
-        <a href="http://" target="_blank" rel="noopener noreferrer">
-        <img src="WatsApp" alt="WatsApp" />
-      </a>
+          <a href="https://wa.me/+2348138780831?text=This%20is%20a%20test" target="_blank" rel="noopener noreferrer">
+          <img src="WatsApp" alt="WatsApp" />
+          </a>
         </div>
         <div className="linkedIn-Wrapper">
-        <a href="http://" target="_blank" rel="noopener noreferrer">
+        <a href="https://github.com/julietoge" target="_blank" rel="noopener noreferrer">
         <img src="GitHub" alt="GitHub" />
       </a>
         </div>
