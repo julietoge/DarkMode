afab71563a9a97935859d91ef5df45e66cbb8926
julietoge <julietogechi27@gmail.com>
2023-11-10 14:06:31 +0100

    installed react-router-dom and framer-motion

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import "./App.css";
 import Header from "./pages/header/header";
 import Mainsection from "./pages/mainsection/mainsection";
 import Footer from "./pages/footer/footer";
-
+import { Routes, Route } from 'react-router-dom'
 const App = () => {
   return (
     <div className="App">
       <Header className="header" />
       <Mainsection className="mainSection" />
       <Footer className="footer" />
+
     </div>
   );
 };
